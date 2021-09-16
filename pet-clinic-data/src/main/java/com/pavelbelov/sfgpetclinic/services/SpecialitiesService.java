a3e0e318134dc5ea13e62462204cdd5e4daeec0d
Nagibator1337-xakep <MamkuIbalAzaza1337>
2021-09-16 21:02:38 +0500

    Created Specialty Map service implementation. Closes #31

---
+++ pet-clinic-data/src/main/java/com/pavelbelov/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,9 @@
+package com.pavelbelov.sfgpetclinic.services;
+
+import com.pavelbelov.sfgpetclinic.model.Speciality;
+
+/**
+ * Created by Pavel Belov on 16.09.2021
+ */
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
