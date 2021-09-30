14ad4e519f14573d1c7f4d0a0e675298a3deee0d
Nagibator1337-xakep <MamkuIbalAzaza1337>
2021-09-30 22:58:52 +0500

    Setting up Vet and Speciality as JPA Entities. Closes #37

--- pet-clinic-data/src/main/java/com/pavelbelov/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/pavelbelov/sfgpetclinic/model/Speciality.java
@@ -1,10 +1,17 @@
 package com.pavelbelov.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by Pavel Belov on 13.09.2021
  */
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/com/pavelbelov/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/pavelbelov/sfgpetclinic/model/Vet.java
@@ -1,14 +1,22 @@
 package com.pavelbelov.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  *  Created by Pavel Belov on 03.09.2021
  */
-
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    // By default, the FetchType is LAZY which means it is only loading when it's called, EAGER fetch type
+    // loads all the data at initialization (as far as I understand)
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
